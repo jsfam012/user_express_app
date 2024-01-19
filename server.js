4683ebb0379ea327531c9f3d3d43c91526b08eda
Sean Ahn <edotakeout@gmail.com>
2024-01-19 13:22:22 -0500

    jd code

--- server.js
+++ server.js
@@ -1,79 +1,167 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors');
+const { v4 } = require('uuid');
 
 const PORT = 3333;
 
 const app = express();
 
 async function getUserData() {
-    const users = await fs.promises.readFile('./data.json', 'utf8');
+  const users = await fs.promises.readFile('./data.json', 'utf8');
 
-    return JSON.parse(users);
+  return JSON.parse(users);
 }
 
 async function saveUserData(usersArr) {
-    await json.promises.writeFile('./data.json', JSON.stringify(usersArr, null, 2));
+  await fs.promises.writeFile('./data.json', JSON.stringify(usersArr, null, 2));
 
-    console.log('User Data Updated');
+  console.log('User Data Updated');
 }
-//Opening up the middleware channel to allow json to be sent through from the client
+
+// Opening up the middleware channel to allow json to be sent through from the client
 app.use(express.json());
 
 // Share or create a GET route for every file in the public folder
 app.use(express.static('./public'));
 
 // Open CORS to all domains
-app.use(cors());
+// app.use(cors());
 
-
-
-// // Root Route
-// app.get('/', (requestObj, responseObj) => {
-//     responseObj.sendFile(path.join(__dirname, './public/index.html'));
-// });
-
-// // CSS File GET Route
-// app.get('/css/style.css', (requestObj, responseObj) => {
-//     responseObj.sendFile(path.join(__dirname, './public/css/style.css'))
-// });
-
-// Route to retrive/GET all users from the json database
+// Route to retreive/GET all users from the json database
 app.get('/api/users', async (requestObj, responseObj) => {
-    //Read the json file data
-    const users = await getUserData();
+  // Read the json file data
+  const users = await getUserData();
 
-    responseObj.send(users);
+  responseObj.send(users);
 });
 
 // Route to add a user to the json database
 app.post('/api/users', async (requestObj, responseObj) => {
-    // Get the old users array
-    const users = await getUserData();
-   
-    //Overwrite the old array with the newly updated aray
-    if (!users.find(uswer => users.username === requestObj.body.username) && requestObj.body.username) {
-        
-        // Push the body object from the client to our old array
-        users.push(requestObj.body);
-        await saveUserData(users);
-        
-        return responseObj.send({
-            message: 'User added successfully!'
-        });
+  // Get the old users array
+  const users = await getUserData();
+  const userData = requestObj.body;
 
-      }   
-    
+  // Overwrite the old array with the newly updated array
+  if (!users.find(user => user.username === userData.username) && userData.username) {
+    // Push the body object from the client to our old array
 
-    responseObj.send({
-        error: 402,
-        message: 'User already exists'
+    userData.id = v4();
+
+    users.push(userData);
+
+    await saveUserData(users);
+
+    return responseObj.send({
+      message: 'User added successfully!'
     });
+  }
+
+  responseObj.send({
+    error: 402,
+    message: 'User already exists'
+  });
+
 });
-
-
 
 app.listen(PORT, () => {
-    console.log('Server started on port', PORT);
+  console.log('Server started on port', PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const path = require('path');
+// const fs = require('fs');
+// // const cors = require('cors');
+// const { v4 } = require('uuid');
+// const PORT = 3333;
+
+// const app = express();
+
+// async function getUserData() {
+//     const users = await fs.promises.readFile('./data.json', 'utf8');
+
+//     return JSON.parse(users);
+// }
+
+// async function saveUserData(usersArr) {
+//     await json.promises.writeFile('./data.json', JSON.stringify(usersArr, null, 2));
+
+//     console.log('User Data Updated');
+// }
+// //Opening up the middleware channel to allow json to be sent through from the client
+// app.use(express.json());
+
+// // Share or create a GET route for every file in the public folder
+// app.use(express.static('./public'));
+
+// // Open CORS to all domains
+// // app.use(cors());
+
+
+
+// // // Root Route
+// // app.get('/', (requestObj, responseObj) => {
+// //     responseObj.sendFile(path.join(__dirname, './public/index.html'));
+// // });
+
+// // // CSS File GET Route
+// // app.get('/css/style.css', (requestObj, responseObj) => {
+// //     responseObj.sendFile(path.join(__dirname, './public/css/style.css'))
+// // });
+
+// // Route to retrive/GET all users from the json database
+// app.get('/api/users', async (requestObj, responseObj) => {
+//     //Read the json file data
+//     const users = await getUserData();
+
+//     responseObj.send(users);
+// });
+
+// // Route to add a user to the json database
+// app.post('/api/users', async (requestObj, responseObj) => {
+//     // Get the old users array
+//     const users = await getUserData();
+//     const userData + requestObj.body;
+   
+//     //Overwrite the old array with the newly updated aray
+//     if (!users.find(uswer => users.username === userDatausername) && userData.username) {
+        
+//     // Push the body object from the client to our old array
+
+//     userData.id = v4();
+
+//     users.push(userData);
+    
+//     await saveUserData(users);
+        
+//     return responseObj.send({
+//         message: 'User added successfully!'
+//         });
+
+//     }   
+    
+
+//     responseObj.send({
+//         error: 402,
+//         message: 'User already exists'
+//     });
+// });
+
+
+
+// app.listen(PORT, () => {
+//     console.log('Server started on port', PORT);
+// });
