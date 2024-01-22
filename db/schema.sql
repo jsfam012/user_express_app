04b58209226a6ae97fd6a0d9afcb19d09131d6f4
Sean Ahn <edotakeout@gmail.com>
2024-01-22 15:07:08 -0500

    started mysql

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS mysql_first_day_db;
+
+CREATE DATABASE mysql_first_day_db;
+
+USE mysql_first_day_db;
+
+CREATE TABLE users (
+    username VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
+
+INSERT INTO users (username, email, password) VALUES
+    ('JD', 'jd@test.com', 'password123'),
+    ('Bob', 'bob@test.com', 'password123'),
+    ('Jane', 'jane@test.com', 'password123');
+
+SELECT * FROM users;
