29f699adedcc499c35972cc7633a8fcbd5599243
Sean Ahn <edotakeout@gmail.com>
2024-01-24 12:53:13 -0500

    ready for deploy

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -7,17 +7,18 @@ const db = require('../db/connection')
 // const { getUserData, saveUserData } = require('../db');
   
 // localhost:333/api/users
-
-
 // Route to retreive/GET all users from the json database
 router.get('/users', async (requestObj, responseObj) => {
   
   // Make a query to the db and get all rows from the users table
-  db.query('SELECT * FROM users', (err, users) => { 
-    if (err) return console.log(err);
+  try {
+    const [users] = await db.query('SELECT * FROM users');
 
     responseObj.json(users);
-  });
+  } catch (err) {
+      console.log(err);
+  }
+
 });
 
   // Route to add a user to the json database
@@ -26,17 +27,31 @@ router.get('/users', async (requestObj, responseObj) => {
     // Get the old users array
     const userData = requestObj.body;
     
+    try {
+    // Check if the user already exists
+    const [results] = await db.query('SELECT * FROM users WHERE username = ?', [userData.username]);
+
+    // Check if a user was found matching that username
+    if (results.length) {
+      return responseObj.json({
+        error: 402,
+        message: 'That user already exists'
+      });
+    }
+
     // Run a query to INSERT a new user into the users table with our requestObj.body data(username, email, password)
-
-    db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', 
-    [userData.username, userData.email, userData.password], (err, results) => {
-      if (err) return console.log(err);
-
-      responseObj.json({
-        message: 'User added successfully!',
-        insertId: results.insertId
-      })
+    const [result] = await db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', 
+    [userData.username, userData.email, userData.password]);
+    
+    responseObj.json({
+      message: 'User added successfully',
+      insertId: result.intertId
     });
+  
+  } catch (err) {
+    console.log(err);
+  } 
+});
     
   
     // responseObj.send({
@@ -44,44 +59,45 @@ router.get('/users', async (requestObj, responseObj) => {
     //   message: 'User already exists'
     // });
   
-  });
+
   
   // GET Route to return a user by ID
   router.get('/users/:id', async (requestObj, responseObj) => {
-      const user_id = requestObj.params.id;
+    const user_id = requestObj.params.id;
 
-      db.query('SELECT * FROM users WHERE id=?', [user_id], (err, user) => {
-        if (err) return console.log(err);
-
-        if (user) {
+    try {
+      const [results] = await db.query('SELECT * FROM users WHERE id=?', [user_id]);
+  
+        if (results.length) {
           return responseObj.json(results[0]);
         }
-
+         
         responseObj.json({
           error: 404,
-          message: 'User not found with that'
-        })
+          message: 'User not found with that ID'
+          });
 
-      });
+    } catch(err) {
+      console.log(err);
+    }
   });
 
   // DELETE Route to remove a user from the database
   router.delete('/user/:id', async (requestObj, responseObj) => {
     // Get the user data
-    const users = await getUserData();
     const user_id = requestObj.params.id;
     
-    //Find the user in the user in the users array matching the param id 
-    const filtered = users.find(usrObj => usrObj.id !== user_id);
-
-    
-    // Overwrite the old array with the updated array (missing the user object)
-    await saveUserData(filtered);
-
-    responseObj.send({
-        message: 'User deleted successfully!'
-    });
-
+    try {
+      // Run a query to delete a user row from the table by user_id
+      await db.query('DELETE FROM users WHERE id = ?', [user_id]);
+      
+      responseObj.send({
+          message: 'User deleted successfully!'
+        });
+      } catch(err) {
+        console.log(err);
+      }    
+          
   });
 
   module.exports = router;
