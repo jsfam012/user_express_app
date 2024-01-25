892550565735ff367874c7604c34dcba50273f55
Sean Ahn <edotakeout@gmail.com>
2024-01-25 10:28:33 -0500

    ready to deploy

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const is_prod = process.env.NODE_ENV === 'production';
-
-
 
 // Create connection to our mysql database
 const connection = mysql.createConnection({
     host: process.env.DB_HOST_URL,
-    user: process.env.DB_USER
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE_NAME
   });
 
